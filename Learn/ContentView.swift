80859e1f13475db4d1597f360f82bf309d1760a3
Sade Nesmith <sadework21@gmail.com>
2024-07-19 10:23:02 -0400

    list

--- Learn/ContentView.swift
+++ Learn/ContentView.swift
@@ -8,22 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+
   //storage for data update user interface using data
   //ui reflects what is in state
   //must be in sync to get swiftui working correctly
-  @State private var isOn: Bool = false
+  @State private var name: String = ""
+  @State private var friends: [String] = []
 
 
-    
-    var body: some View {
-      VStack {
-        Toggle(isOn: $isOn, label: {
-          Text(isOn ? "ON": "OFF")
-        }).fixedSize()
-      }.frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(isOn ? .yellow: .purple)
-    }
+
+  var body: some View {
+    VStack {
+      TextField("Enter name", text: $name)
+        .textFieldStyle(.roundedBorder)
+        .onSubmit {
+          friends.append(name)
+          name = ""
+        }
+
+      //display frineds array
+      List(friends, id: \.self) { friend in
+        Text(friend)
+      }
+
+
+      Spacer()
+    }.padding()
+  }
 }
 
 #Preview {
